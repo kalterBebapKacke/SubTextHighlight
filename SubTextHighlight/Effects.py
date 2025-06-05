edb1d5b91f269892596499ff0330c148658f5dea
moonlitmarigold <moonlitmarigold12@gmail.com>
2025-06-05 20:48:42 +0200

    Added base content

---
+++ SubTextHighlight/Effects.py
@@ -0,0 +1,35 @@
+import os
+from os import fdatasync
+
+import pysubs2
+
+class effects_args:
+
+    def __init__(self,
+        fade:tuple[float, float] = (0.0, 0.0) # first is fadeIn and second is fadeOut
+                 ):
+        self.fade_in_duration = fade[0]
+        self.fade_out_duration = fade[1]
+
+
+class Effects:
+
+    def __init__(self, args:effects_args):
+        self.args = args
+
+    def __call__(self, subs):
+        if self.args.fade_out_duration != 0 and self.args.fade_in_duration != 0:
+            subs = self.fade(subs)
+        else:
+            subs = subs
+        return subs
+
+    def fade(self, subs):
+        for i, sub in enumerate(subs):
+            if type(sub) == list:
+                sub[0].text = fr'{{\fad({self.args.fade_in_duration},0)}}{sub[0].text}'
+                sub[-1].text = fr'{{\fad(0,{self.args.fade_out_duration})}}{sub[-1].text}'
+            else:
+                sub.text = fr'{{\fad({self.args.fade_in_duration},{self.args.fade_out_duration})}}{sub.text}'
+        return subs
+
