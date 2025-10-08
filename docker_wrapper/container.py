abbbaa98c55718829d56b0bce6ececef9c475dd6
kalterBebapKacke <walterwurstbauer@gmail.com>
2025-10-08 20:46:21 +0200

    Committing current development progress. Added important variables for the configuration and finishing calling logic

--- docker_wrapper/container.py
+++ docker_wrapper/container.py
@@ -1,6 +1,7 @@
 from . import base
 import tarfile
 import tempfile
+from io import BytesIO
 
 class ContainerWrapper(base.BaseWrapper):
 
@@ -14,7 +15,7 @@ class ContainerWrapper(base.BaseWrapper):
             self.verbose = verbose
             self.container = container
 
-    def __call__(self, command: list | str):
+    def __call__(self, command: list | str, workdir:str='/home'):
         # Check if container is running
         self.container_running()
         # Check if the command is in the right format
@@ -22,57 +23,81 @@ class ContainerWrapper(base.BaseWrapper):
             command = self.build_command(command)
         # add exec to command
         command = ['bash', '-c', command]
-        print(command)
-        exit_code, output = self.container.exec_run(command, workdir='/home')
+        exit_code, output = self.container.exec_run(command, workdir=workdir)
         if exit_code != 0:
             raise RuntimeError(f'Container run into the following error with exit code {exit_code}: {output}')
         if self.verbose:
-            print(output)
+            if output != '' or output != '\n':
+                print('-------------------')
+                print(output.decode("utf-8"))
+                print('-------------------')
 
     def container_running(self):
         # loop for waiting container to start?
         self.container.reload()
 
         # Check if it's running
-        print(self.container.status)
         if self.container.status != 'running':
             raise RuntimeError('Container is not running. It needs to be started first.')
 
     def build_command(self, commands: list[str]):
         return ' && '.join(commands)
 
-    def install_fonts(self, fonts_path:list | str,):
-        if type(fonts_path) is str:
-            fonts_path = [fonts_path]
+    def copy_needed_files(self, input_ass_path:str, fonts_path:list | str = None):
+        # Add if fonts should be copied
+        if fonts_path is None:
+            if_copy_fonts = False
+        else:
+            if_copy_fonts = True
+            if type(fonts_path) is str:
+                fonts_path = [fonts_path]
 
-        # Build tar for attaching in the container
-        # Tmp file
         with tempfile.NamedTemporaryFile(suffix='.tar.gz', delete=True) as tmp:
-            # Create actual tar dir
+
             with tarfile.open(fileobj=tmp, mode='w:gz') as tar:
-                for font in fonts_path:
-                    if not font.endswith(".ttf"):
-                        print(f'Font "{font}" is not a ttf file')
-                    else:
-                        tar.add(font, recursive=False)
+                # Only add fonts if they are wanted
+                if if_copy_fonts:
+                    for font in fonts_path:
+                        if not font.endswith(".ttf"):
+                            print(f'Font "{font}" is not a ttf file')
+                        else:
+                            tar.add(font, arcname=f'fonts/{font.split("/")[-1]}')
+                # Add input ass
+                tar.add(input_ass_path, arcname='input.ass')
 
             # Flush and seek back to the beginning
             tmp.flush()
             tmp.seek(0)
 
+            # import tar in docker
             self.container.put_archive('/home', tmp)
-
-            print(tmp)
-            print(tmp.name)
-
+        # Put fonts in the right directory if they are needed
+        if if_copy_fonts:
             command = [
-                'mkdir -p /fonts',
-                f'tar -xf archive.tar.gz -C /home/fonts',
                 'mv /home/fonts/* /root/.local/share/fonts/',
-                ''
+                'rm -rf /home/fonts'
             ]
-            print(self.build_command(command))
-            self(['ls'])
+            self(command)
+
+    def install_packages(self, packages:list[str]):
+        command = ['apt-get update', f'apt-get install -y --no-install-recommends {' '.join(packages)}', 'rm -rf /var/lib/apt/lists/*']
+        self(command)
+
+    def retrieve_file(self, path:str, return_content:bool=False):
+        bits, stat= self.container.get_archive(path)
+        if return_content:
+            buffer = BytesIO()
+            for chunk in bits:
+                buffer.write(chunk)
+            buffer.seek(0)
+            return buffer.read().decode('utf-8-sig').strip().lstrip('\x00\ufeff')
+        else:
+            return bits
+
+
+
+
+
 
 
 
