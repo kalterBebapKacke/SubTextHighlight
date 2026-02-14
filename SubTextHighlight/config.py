0b03c7710f70815341475134b389f46d93d3f09e
moonlitmarigold <moonlitmarigold12@gmail.com>
2026-02-14 09:18:04 +0100

    Fully reworked highlight args

--- SubTextHighlight/config.py
+++ SubTextHighlight/config.py
@@ -23,13 +23,13 @@ class SubtitleConfig:
     fill_sub_times: bool = True
 
     # highlight styles
-    highlight_word_max: int | None = 0
-    highlight_style: StyleConfig = field(default_factory=StyleConfig)
-    highlight_as_borders: bool = False
+    highlight_word_max: int | None = None
+    highlight_style: StyleConfig | None = None
+    highlight_as_borders: bool | None = None
 
     # effect args
-    fade: tuple[float, float] = (0.0, 0.0)
-    appear: bool = False
+    fade: tuple[float, float] | None = None #(0.0, 0.0)
+    appear: bool | None = None
 
     # border args
     offset: int = 6
@@ -56,18 +56,15 @@ class SubtitleConfig:
 
     @property
     def is_effect_needed(self):
-        if self.highlight_as_borders:
-            return False
-        if self.fade != (0.0, 0.0):
-            return False
-        if self.appear:
+        if self.highlight_as_borders is None and self.fade is None and self.appear is None:
             return False
         return True
 
     @property
     def is_highlighter_needed(self):
-        # TODO: implement logic
-        return None
+        if self.highlight_as_borders is None and self.highlight_word_max is None and self.highlight_style is None:
+            return False
+        return True
 
     def __post_init__(self):
         self.args_border = docker_wrapper.base.args_border(
@@ -92,26 +89,34 @@ class SubtitleConfig:
             whisper_refine=self.whisper_refine,
         )
 
-        #self.highlighter = Highlight.Highlighter()
 
-    def render(self):
-        # get subfile
-        sub_file = self.input_handler.handle_input()
-
-        # set main style
-        main_style = self.subtitle_style.return_style()
-        sub_file.styles["MainStyle"] = main_style
-
-        # set highlighter
-        self.highlighter = self.highlighter_logic(main_style)
-
-    def highlighter_logic(self, main_style:pysubs2.SSAStyle):
+    def highlighter_logic(self, main_style: pysubs2.SSAStyle):
         # TODO: Rework highlighter to work
-        highlighter = Highlight.Highlighter()
+        if self.highlight_word_max is None:
+            self.highlight_word_max = 0
+        if self.highlight_style is None:
+            self.highlight_style = StyleConfig()
+
+        highlighter = Highlight.Highlighter(self.highlight_word_max, self.subtitle_type)
 
         if not self.is_highlighter_needed:
             if not self.is_effect_needed:
                 return None
 
         return highlighter
+
+    def render(self):
+        # get subfile and set highlighter
+        sub_file = self.input_handler.handle_input()
+
+        # set main style
+        main_style = self.subtitle_style.return_style()
+        sub_file.styles["MainStyle"] = main_style
+
+        # set highlighter and style
+        self.highlighter = self.highlighter_logic(main_style)
+        sub_file.styles["Highlight"] = self.highlight_style.compare_style(main_style)
+
+
+
 
