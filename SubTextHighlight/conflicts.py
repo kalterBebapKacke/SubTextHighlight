e9b7b083508e6053a2ca3404ce2b8fdd23ab105b
moonlitmarigold <moonlitmarigold12@gmail.com>
2026-05-28 20:27:56 +0200

    New conflict checker and a slight adjust to the way the pipline is build

--- SubTextHighlight/conflicts.py
+++ SubTextHighlight/conflicts.py
@@ -1,2 +1,26 @@
-# appear
-# background stuff
+def build_pipeline_check_conflicts(config):
+
+    if config.highlight_as_borders and config.rounded_border:
+        raise ValueError("highlight_as_borders and rounded_border cannot both be True.")
+
+    if config.rounded_border or config.highlight_as_borders:
+        if config.offset <= 0:
+            raise ValueError("offset must be greater than 0 for borders.")
+        if config.radius <= 0:
+            raise ValueError("radius must be greater than 0 for borders.")
+        if config.transformy <= 0:
+            raise ValueError("transformy must be greater than 0 for borders.")
+        if config.height_scaling <= 0:
+            raise ValueError("height_scaling must be greater than 0 for borders.")
+
+class PipelineConflictChecker:
+
+    def __init__(self, config):
+        self.config = config
+
+    def check_conflicts(self, sub_file):
+        if self.config.rounded_border or self.config.highlight_as_borders:
+            if sub_file.resolution is None:
+                raise RuntimeError('The subtitle file does not contain a Resolution. For the right scaling of the subtitles a input with a video resolution has to be set.')
+
+        return sub_file
