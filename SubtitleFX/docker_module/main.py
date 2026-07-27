0cfc6768677b78afcb0513b7a8669bb630b04c85
moonlitmarigold <moonlitmarigold12@gmail.com>
2026-07-27 22:16:14 +0200

    New Main Docker Wrapper

--- SubtitleFX/docker_module/main.py
+++ SubtitleFX/docker_module/main.py
@@ -5,7 +5,8 @@ from typing import Optional
 import logging
 import pysubs2
 import dataclasses
-
+import json
+import tempfile
 logger = logging.getLogger(__name__)
 
 @dataclasses.dataclass
@@ -13,7 +14,66 @@ class DockerWrapper(base.BaseWrapper):
 
     container: Optional[Container] = None
 
+    input_path = 'input.ass'
+    output_path = 'output.ass'
+
     def __call__(self, sub_file:pysubs2.SSAFile, border_config:utils.BorderConfig):
         if not self.container:
-            self.container = Container.__return_base_container(self.config)
+            container = Container.__return_base_container(self.config)
+        else:
+            container = self.container
+
+        # cleanup of maybe old files
+        logger.debug('Cleaning up old files')
+        cleanup = [
+            f'rm -rf {self.input_path}',
+            f'rm -rf {self.output_path}',
+        ]
+        container(cleanup)
+
+        # place the sub_file
+        logger.debug('Placign the subfiles')
+        with container.archive_writer() as tar:
+            with tempfile.NamedTemporaryFile(suffix='.ass', delete=True) as ass:
+                sub_file.save(ass.name)
+                tar.add(ass.name, arcname=self.input_path)
+
+        dialog_json = (
+            json.dumps(
+                {
+                    "button": 0,
+                    "values": border_config.json_info()
+                }
+            )
+        )
+
+        shapery_command = [
+            'aegisub-cli',
+            f"--dialog '{dialog_json}'",
+            '--automation',
+            'jz.RoundedBorders.lua',
+            self.input_path,
+            self.output_path,
+            '"Create Rounded Border"',
+            '|| true'
+        ]
+
+        logger.debug('Executing Shapery Command')
+        container(' '.join(shapery_command))
+        logger.debug('Executing Shapery Finished')
+
+        output: str = container.get_output(f'/home/{self.output_path}')
+        output: str = output[output.find('[Script Info]'):]
+
+        output_subfile = pysubs2.SSAFile.from_string(output)
+
+        logger.debug(f'Output from the Docker:\n{output_subfile.to_string('ass')}')
+
+        container.set_pause()
+
+        return output_subfile
+
+
+
+
 
