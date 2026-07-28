d168d4f83a6986b8d2ddffe636107e2cbb172b40
moonlitmarigold <moonlitmarigold12@gmail.com>
2026-07-28 19:45:33 +0200

    Tests for new docker

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -1,4 +1,5 @@
-from SubtitleFX import docker_wrapper
+import SubtitleFX
+from SubtitleFX import docker_module
 import pysubs2
 from pathlib import Path
 import os
@@ -12,18 +13,18 @@ def test_docker():
     ass_path = base_path / "input" / "docker_sub_file.ass"
     expected_file = base_path / "expected" / "docker_sub_file.ass"
 
-    dw = docker_wrapper.main.DockerWrapper()
-    args_border = docker_wrapper.base.args_border(
-        fonts_path=[str(arial_path), str(petemoss_path)],
+    docker_config = SubtitleFX.DockerConfig(
+        force_install=True,
+        fonts_path=[arial_path, petemoss_path]
     )
+
+    dw = docker_module.DockerWrapper(docker_config)
+
 
     input_ass = pysubs2.load(str(ass_path))
     output_file = dw(
-        input_ass=input_ass,
-        _traceback=True,
-        cleanup=True,
-        verbose=True,
-        args_border=args_border,
+        input_ass,
+        SubtitleFX.BorderConfig()
     )
     if UPDATE_GOLDEN:
         output_file.save(str(expected_file))
@@ -33,4 +34,33 @@ def test_docker():
         print(output_file.to_string('ass'))
         assert expected_sub_file.to_string('ass') == output_file.to_string('ass')
 
+def test_docker_container():
+    base_path = Path("")
+    arial_path = base_path / "input" / "Arial-Rounded-MT-Bold-Bold.ttf"
+    petemoss_path = base_path / "input" / "Petemoss-Regular.ttf"
+    ass_path = base_path / "input" / "docker_sub_file.ass"
+    expected_file = base_path / "expected" / "docker_sub_file.ass"
+
+    docker_config = SubtitleFX.DockerConfig(
+        force_install=True,
+        fonts_path=[arial_path, petemoss_path]
+    )
+
+    container = SubtitleFX.docker_module.Container.return_base_container(docker_config)
+
+    dw = docker_module.DockerWrapper(docker_config, container=container)
+
+
+    input_ass = pysubs2.load(str(ass_path))
+    output_file = dw(
+        input_ass,
+        SubtitleFX.BorderConfig()
+    )
+    if UPDATE_GOLDEN:
+        output_file.save(str(expected_file))
+        assert True
+    else:
+        expected_sub_file = pysubs2.load(str(expected_file))
+        print(output_file.to_string('ass'))
+        assert expected_sub_file.to_string('ass') == output_file.to_string('ass')
 
